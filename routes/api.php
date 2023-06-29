05a10947731bff86e3327939c1ac4653b12ea303
Raylla Luiza <raylla_op12@hotmail.com>
2023-06-29 18:34:59 -0300

    criando e removendo rotas

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\APIStatusController;
+use App\Http\Controllers\API\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('status', [APIStatusController::class, 'index']);
+Route::apiResource('produtos', ProductController::class)->except('store');
